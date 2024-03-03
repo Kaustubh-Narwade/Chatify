c06e9e38663442ddd4aa14406a9427621dfd73d5
Kaustubh-Narwade <kaustubh.narwade11@gmail.com>
2024-03-03 19:08:05 +0530

    vhost

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,8 +1,8 @@
 export const host = "http://localhost:5000";
 export const vhost = "https://chatify-nine.vercel.app";
-export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${host}/api/auth/login`;
-export const setAvatarRoute = `${host}/api/auth/setavatar`;
-export const allUsersRoute = `${host}/api/auth/allusers`;
-export const sendMessageRoute = `${host}/api/messages/addmsg`;
-export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
+export const registerRoute = `${vhost}/api/auth/register`;
+export const loginRoute = `${vhost}/api/auth/login`;
+export const setAvatarRoute = `${vhost}/api/auth/setavatar`;
+export const allUsersRoute = `${vhost}/api/auth/allusers`;
+export const sendMessageRoute = `${vhost}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${vhost}/api/messages/getmsg`;
